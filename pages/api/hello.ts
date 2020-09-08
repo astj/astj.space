b26b3bfa3f86a939884563f57b822056f09d3fd4
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-09-09 00:37:33 +0900

    improve typings using https://nextjs.org/learn/excel/typescript/nextjs-types

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ text: 'Hello' })
 }
