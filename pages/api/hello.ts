d4d32bb959092b15d1c2f68e95dc7742ed23cfec
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-09-09 01:01:13 +0900

    typings

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default (_: NextApiRequest, res: NextApiResponse) => {
+const apiHandler: NextApiHandler = (
+  _: NextApiRequest,
+  res: NextApiResponse
+) => {
   res.status(200).json({ text: "Hello" });
 };
+
+export default apiHandler;
